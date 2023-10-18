2632b840d9daad4fa8810cdbd131c1cd50ccdfc9
Valdemir Junior <valdemirvoltatoni@gmail.com>
2023-10-18 20:39:38 -0300

    primeira_alteracao

--- src/main/java/org/example/Busca_clima.java
+++ src/main/java/org/example/Busca_clima.java
@@ -14,7 +14,7 @@ public class Busca_clima {
         public Consulta getClima(String lat) throws IOException {
             Consulta resultado = null;
             String lon = "-53.1940876";
-            HttpGet request = new HttpGet("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=f24be9766068168b900f6f492f525a53&units=metric");
+            HttpGet request = new HttpGet("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=%Token%&units=metric");
 
             try (CloseableHttpClient httpClient = HttpClientBuilder.create().disableRedirectHandling().build();
                  CloseableHttpResponse response = httpClient.execute(request)) {
